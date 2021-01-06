3d26021ee2461603f50f5704d47e112c02e2c7f5
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-06 14:15:48 +0800

    Add initial code to process task info

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,23 @@
+use std::net::UdpSocket;
+
+const MAX_BUFFER_SIZE: usize = 1000;
+
+pub fn process_task_info(task: &[u8], address: &str) {
+	if let Ok(_socket) = UdpSocket::bind(address) {
+		todo!("Create the server")
+	} else {
+		let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+		socket.connect(address).unwrap();
+		socket.send(task).unwrap();
+		let mut buffer = [0; MAX_BUFFER_SIZE];
+		let _result_size = socket.recv(&mut buffer).unwrap();
+		todo!()
+	}
+}
+
 #[cfg(test)]
 mod t {
-	use std::net::UdpSocket;
+	use super::UdpSocket;
 
 	const TARGET_SEND_SIZE: usize = 10;
 	const TARGET_PREPARED_RECEIVE_SIZE: usize = 20;
