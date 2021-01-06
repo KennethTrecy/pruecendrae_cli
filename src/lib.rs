fc980802e587888835d299357193a4a8bee977d4
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-06 14:30:31 +0800

    Add initial code to spawn server

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::net::UdpSocket;
+use std::net::{UdpSocket, ToSocketAddrs};
+use std::thread::{JoinHandle, spawn};
 
 const MAX_BUFFER_SIZE: usize = 1000;
 
@@ -13,6 +14,21 @@ pub fn process_task_info(task: &[u8], address: &str) {
 		let _result_size = socket.recv(&mut buffer).unwrap();
 		todo!()
 	}
+}
+
+pub fn spawn_server(address: impl ToSocketAddrs) -> JoinHandle<()> {
+	let socket = UdpSocket::bind(address).unwrap();
+	spawn(move || {
+		let mut buffer = [0; MAX_BUFFER_SIZE];
+
+		loop {
+			let (size, _address) = socket.recv_from(&mut buffer).unwrap();
+			let _request = &buffer[0..size];
+			break;
+		}
+
+		todo!();
+	})
 }
 
 #[cfg(test)]
