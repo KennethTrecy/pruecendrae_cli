0e56c030cd37d9449030e6045b774a2b0acc50bb
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-09 13:54:06 +0800

    Parse create request

--- src/parse_requests.rs
+++ src/parse_requests.rs
@@ -1,12 +1,17 @@
 use chearmyp::parse::{Node, parse};
 use crate::request::Request;
 
+mod parse_create_request;
+
+use parse_create_request::parse_create_request;
+
 pub fn parse_requests(src: &[u8]) -> (Vec<Result<Request, ()>>, bool) {
 	let nodes = parse(src);
 	let mut requests = Vec::with_capacity(nodes.len());
 
 	for node in nodes {
-		let request: Result<Request, ()> = match node {
+		let request = match node {
+			Node::Complex(b"create", _, tasks) => parse_create_request(tasks),
 			_ => todo!()
 		};
 		requests.push(request);
@@ -17,4 +22,22 @@ pub fn parse_requests(src: &[u8]) -> (Vec<Result<Request, ()>>, bool) {
 	});
 
 	(requests, are_all_ok)
+}
+
+#[cfg(test)]
+mod t {
+	use super::{parse_requests, Request};
+
+	#[test]
+	pub fn can_parse_create_request() {
+		let sample = b"create\n\ttask A|\n\tcommand: task_a";
+
+		let parsed_requests = parse_requests(sample);
+
+		assert_eq!(parsed_requests, (vec![
+			Ok(
+				Request::Create(vec![("task A", b"task_a")])
+			)
+		], true));
+	}
 }
---
+++ src/parse_requests/parse_create_request.rs
@@ -0,0 +1,82 @@
+use std::str::from_utf8;
+use chearmyp::parse::Node;
+use crate::request::Request;
+
+pub fn parse_create_request(tasks: Vec<Node>) -> Result<Request, ()> {
+	let mut parsed_tasks = Vec::new();
+
+	for task in tasks {
+		if let Node::Simplex(name, infos) = task {
+			let mut command = None;
+			for info in infos {
+				if let Node::Attacher(label, content) = info {
+					match label {
+						b"command" => command = Some(content),
+						_ => todo!()
+					}
+				}
+			}
+
+			let name = from_utf8(name).unwrap();
+			let command = command.unwrap();
+			let info = (name, command);
+			parsed_tasks.push(info);
+		} else {
+			todo!()
+		}
+	}
+
+	Ok(Request::Create(parsed_tasks))
+}
+
+#[cfg(test)]
+mod t {
+	use super::{Node, parse_create_request, Request};
+
+	macro_rules! test {
+		($test_name:ident using $sample:expr, expecting $expected_data:expr) => {
+			#[test]
+			fn $test_name() {
+				let sample = $sample;
+
+				let tasks = parse_create_request(sample);
+
+				assert_eq!(tasks, $expected_data);
+			}
+		};
+	}
+
+	test!(
+		can_parse_one_task
+		using vec![
+			Node::Simplex(b"task name", vec![
+				Node::Attacher(b"command", b"task command")
+			])
+		],
+		expecting Ok(
+			Request::Create(
+				vec![("task name", b"task command")]
+			)
+		)
+	);
+
+	test!(
+		can_parse_multiple_tasks
+		using vec![
+			Node::Simplex(b"task name A", vec![
+				Node::Attacher(b"command", b"task command A")
+			]),
+			Node::Simplex(b"task name B", vec![
+				Node::Attacher(b"command", b"task command B")
+			])
+		],
+		expecting Ok(
+			Request::Create(
+				vec![
+					("task name A", b"task command A"),
+					("task name B", b"task command B")
+				]
+			)
+		)
+	);
+}
