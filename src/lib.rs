89272c632b8af53cd3dab61055a7f871537010bd
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-09 14:30:33 +0800

    Process requests to the server

--- src/lib.rs
+++ src/lib.rs
@@ -19,14 +19,41 @@ pub fn process_task_info(task: &[u8], address: &str) {
 	}
 }
 
+use pruecendrae_core::TaskMaintainer;
+use request::Request;
+use parse_requests::parse_requests;
+
 pub fn spawn_server(address: impl ToSocketAddrs) -> JoinHandle<()> {
 	let socket = UdpSocket::bind(address).unwrap();
 	spawn(move || {
+		let mut maintainer = TaskMaintainer::new();
 		let mut buffer = [0; MAX_BUFFER_SIZE];
 
 		loop {
 			let (size, _address) = socket.recv_from(&mut buffer).unwrap();
-			let _request = &buffer[0..size];
+			let requests = &buffer[0..size];
+			let (requests, are_all_ok) = parse_requests(requests);
+
+			if are_all_ok {
+				let mut present_tasks_per_request = Vec::with_capacity(requests.len());
+
+				for request in requests {
+					let request = request.unwrap();
+					match request {
+						Request::Create(task_infos) => {
+							for task_info in task_infos {
+								let (task, command) = task_info;
+								maintainer.create(task, command).unwrap();
+							}
+						},
+						Request::Maintainer(maintainer_request) => {
+							let present_tasks	 = maintainer.send_request(maintainer_request);
+							present_tasks_per_request.push(present_tasks);
+						}
+					}
+				}
+			}
+
 			break;
 		}
 
